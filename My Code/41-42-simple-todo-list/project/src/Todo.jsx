3b096984c7b6f05d0488cb91a0b6df6d3c253086
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-09 00:47:35 +0600

    done styling

--- My Code/41-42-simple-todo-list/project/src/Todo.jsx
+++ My Code/41-42-simple-todo-list/project/src/Todo.jsx
@@ -4,9 +4,16 @@ import "../styles.css"
 export default function Todo({ name, checked, handleCheck, handleDelete }) {
   console.log(checked)
   return (
-    <li style={{fontFamily:"cursive",listStyleType:"none",padding:"0",margin:"0"}}>
+    <li
+      style={{
+        fontFamily: "cursive",
+        listStyleType: "none",
+        padding: "0",
+        margin: "0",
+      }}
+    >
       <input
-        style={{margin:"5px"}}
+        style={{ margin: "5px" }}
         type="checkbox"
         name="done"
         id="done"
@@ -16,7 +23,20 @@ export default function Todo({ name, checked, handleCheck, handleDelete }) {
       <span style={{ textDecoration: checked ? "line-through" : "none" }}>
         {name}
       </span>
-      <button style={{margin:"5px",backgroundColor:"red",border:"0",padding:"5px 8px",color:"white",borderRadius:"3px", fontWeight:"bold"}} onClick={handleDelete}>Delete</button>
+      <button
+        style={{
+          margin: "5px",
+          backgroundColor: "red",
+          border: "0",
+          padding: "5px 10px",
+          color: "white",
+          borderRadius: "15px",
+          fontWeight: "bold",
+        }}
+        onClick={handleDelete}
+      >
+        Delete
+      </button>
     </li>
   )
 }
