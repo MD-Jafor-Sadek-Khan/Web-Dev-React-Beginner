e226029f2ea333bc5b6fba9e568733a9a90bb78b
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-01-14 21:46:38 +0600

    Done

--- My Code/58-59-basic-form-validation-project/project/src/AppRefv2.jsx
+++ My Code/58-59-basic-form-validation-project/project/src/AppRefv2.jsx
@@ -5,18 +5,19 @@ import "../styles.css"
 export function AppRefv2() {
   const email = useRef("")
   const password = useRef("")
-  const [emailError,setEmailError] = useState([])
-  const [passwordError,setPasswordError] = useState([])
-
+  const [emailError, setEmailError] = useState([])
+  const [passwordError, setPasswordError] = useState([])
+  const [render, setRender] = useState(false)
 
   function handleSubmit(event) {
     event.preventDefault()
+    setRender(true)
 
     const emailResult = emailErrorHandler(email.current.value)
     const passwordResult = passwordErrorHandler(password.current.value)
     setEmailError(emailResult)
     setPasswordError(passwordResult)
-    
+
     if (emailResult.length === 0 && passwordResult.length === 0) {
       alert("Success")
     }
@@ -29,7 +30,17 @@ export function AppRefv2() {
           <label className="label" htmlFor="email">
             Email
           </label>
-          <input className="input" type="email" id="Useremail" ref={email} />
+          <input
+            className="input"
+            type="email"
+            id="Useremail"
+            ref={email}
+            onChange={
+              render ?
+              ((e) => setEmailError(emailErrorHandler(e.target.value)))
+              : undefined
+            }
+          />
           <div className="msg">
             {emailError.length > 0 && emailError.join(", ")}
           </div>
@@ -43,6 +54,11 @@ export function AppRefv2() {
             type="password"
             id="Userpassword"
             ref={password}
+            onChange={
+              render ?
+              ((e) => setPasswordError(passwordErrorHandler(e.target.value)))
+              : undefined
+            }
           />
           <div className="msg">
             {passwordError.length > 0 && passwordError.join(", ")}
